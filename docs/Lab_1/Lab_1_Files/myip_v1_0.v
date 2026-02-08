abd3d281053673ba9257eaf4ff6512c9cc1f2e2c
woodenclock <luckyisland3710@gmail.com>
2026-02-08 21:24:27 +0800

    update for compute in myip

--- docs/Lab_1/Lab_1_Files/myip_v1_0.v
+++ docs/Lab_1/Lab_1_Files/myip_v1_0.v
@@ -1,18 +1,3 @@
-/* 
-----------------------------------------------------------------------------------
---	(c) Rajesh C Panicker, NUS
---  Description : Matrix Multiplication AXI Stream Coprocessor. Based on the orginal AXIS Coprocessor template (c) Xilinx Inc
--- 	Based on the orginal AXIS coprocessor template (c) Xilinx Inc
---	License terms :
---	You are free to use this code as long as you
---		(i) DO NOT post a modified version of this on any public repository;
---		(ii) use it only for educational purposes;
---		(iii) accept the responsibility to ensure that your implementation does not violate any intellectual property of any entity.
---		(iv) accept that the program is provided "as is" without warranty of any kind or assurance regarding its suitability for any particular purpose;
---		(v) send an email to rajesh.panicker@ieee.org briefly mentioning its use (except when used for the course EE4218 at the National University of Singapore);
---		(vi) retain this notice in this file or any files derived from this.
-----------------------------------------------------------------------------------
-*/
 /*
 -------------------------------------------------------------------------------
 --
@@ -73,7 +58,6 @@ module myip_v1_0
 // You will need to modify this example for
 // MODULE myip_v1_0 to implement your coprocessor
 
-
 // RAM parameters for assignment 1
     localparam m = 2;
     localparam n = 4;
@@ -93,13 +77,13 @@ module myip_v1_0
 	reg    	[A_depth_bits-1:0] A_write_address;			// myip_v1_0 -> A_RAM. To be assigned within myip_v1_0. Possibly reg. 
 	reg    	[width-1:0] A_write_data_in;				// myip_v1_0 -> A_RAM. To be assigned within myip_v1_0. Possibly reg.
 	wire    A_read_en;									// matrix_multiply_0 -> A_RAM.
-	wire    [A_depth_bits-1:0] A_read_address = 0;		// matrix_multiply_0 -> A_RAM.
+	wire    [A_depth_bits-1:0] A_read_address;		    // matrix_multiply_0 -> A_RAM.
 	wire	[width-1:0] A_read_data_out;				// A_RAM -> matrix_multiply_0.
 	reg    	B_write_en;									// myip_v1_0 -> B_RAM. To be assigned within myip_v1_0. Possibly reg.
 	reg	    [B_depth_bits-1:0] B_write_address;			// myip_v1_0 -> B_RAM. To be assigned within myip_v1_0. Possibly reg.
 	reg 	[width-1:0] B_write_data_in;				// myip_v1_0 -> B_RAM. To be assigned within myip_v1_0. Possibly reg.
 	wire 	B_read_en;									// matrix_multiply_0 -> B_RAM.
-	wire 	[B_depth_bits-1:0] B_read_address = 0;		// matrix_multiply_0 -> B_RAM.
+	wire 	[B_depth_bits-1:0] B_read_address;		    // matrix_multiply_0 -> B_RAM.
 	wire	[width-1:0] B_read_data_out;				// B_RAM -> matrix_multiply_0.
 	wire 	RES_write_en;								// matrix_multiply_0 -> RES_RAM.
 	wire 	[RES_depth_bits-1:0] RES_write_address;		// matrix_multiply_0 -> RES_RAM.
@@ -217,8 +201,13 @@ module myip_v1_0
                     read_counter    <= 0;
                     A_write_en  <= 0;
                     B_write_en  <= 0;
+                    Start <= 0;  // Deassert Start after 1 cycle
 					// Coprocessor function to be implemented (matrix multiply) should be here. Right now, nothing happens here.
-					state		<= Write_Outputs;
+                    if (Done == 1) begin
+                        state <= Write_Outputs;
+                        RES_read_en <= 1;
+                        RES_read_address <= 0;
+                    end
 					// Possible to save a cycle by asserting M_AXIS_TVALID and presenting M_AXIS_TDATA just before going into 
 					// Write_Outputs state. However, need to adjust write_counter limits accordingly
 					// Alternatively, M_AXIS_TVALID and M_AXIS_TDATA can be asserted combinationally to save a cycle.
